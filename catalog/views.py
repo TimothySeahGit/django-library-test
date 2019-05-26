a5a08734d43044f57264c5abe8f4e6c0aa313f58
unknown <timothyseah.t.e@gmail.com>
2019-05-26 21:18:21 +0800

    Add Staff user permissions for viewing all loaned books

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.views import generic
 from django.shortcuts import render
 
@@ -63,3 +64,14 @@ class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
 
     def get_queryset(self):
         return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
+
+
+class LoanedBooksBySuperUserListView(PermissionRequiredMixin, generic.ListView):
+    """Generic class-based view listing books on loan to current user."""
+    permission_required = 'catalog.can_mark_returned'
+    model = BookInstance
+    template_name = 'catalog/bookinstance_list_borrowed_user.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return BookInstance.objects.filter(status__exact='o').order_by('due_back')
