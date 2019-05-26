25bdaaa5fa14c6fe6b09656eba81befb3016bc01
unknown <timothyseah.t.e@gmail.com>
2019-05-26 17:08:56 +0800

    Add 'view my borrowed books' page

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import generic
 from django.shortcuts import render
 
@@ -31,7 +32,6 @@ def index(request):
         'num_authors': num_authors,
         'num_visits': num_visits,
     }
-    
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
@@ -53,3 +53,13 @@ class AuthorListView(generic.ListView):
 
 class AuthorDetailView(generic.DetailView):
     model = Author
+
+
+class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
+    """Generic class-based view listing books on loan to current user."""
+    model = BookInstance
+    template_name = 'catalog/bookinstance_list_borrowed_user.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
