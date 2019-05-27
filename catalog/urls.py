d8f4cbec6bdfdf566612f4605ab9fe04ad0a1671
Timothy Seah <timothyseah.t.e@gmail.com>
2019-05-27 13:44:51 +0800

    Add renew due date functionality

--- catalog/urls.py
+++ catalog/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
     path('allborrowedbooks/', views.LoanedBooksBySuperUserListView.as_view(),
          name='all-borrowed'),
-
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian,
+         name='renew-book-librarian'),
 ]
